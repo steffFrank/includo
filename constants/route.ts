70151f58a7fa5275ef28103f4b66f0c7ce0fa25c
steffFrank <steffpouassi@gmail.com>
2024-12-08 20:01:51 +0100

    feat: Add constant routes

---
+++ constants/route.ts
@@ -0,0 +1,19 @@
+const ROUTES = {
+    HOME: "/",
+    SIGN_IN: "/sign-in",
+    SIGN_UP: "/sign-up",
+    COURSES: "/courses",
+    BLOG: "/blog",
+    TESTIMONIALS: "/testimonials",
+    CONTACT: "/contact",
+    ADMIN: {
+        DASHBOARD: "/admin/dashboard",
+        COURSES: "/admin/courses",
+        USERS: "/admin/users",
+        SETTINGS: "/admin/settings",
+    },
+    FORGOT_PASSWORD: "/auth/forgot-password",
+    RESET_PASSWORD: "/auth/reset-password",
+};
+
+export default ROUTES;
